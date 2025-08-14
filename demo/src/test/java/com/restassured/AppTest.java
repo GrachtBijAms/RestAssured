1c83869c8867ed2cc210c77f567490660874c26d
Prathamesh Patil <patilprathamesh76@gmail.com>
2025-08-14 17:10:37 +0200

    first commit

---
+++ demo/src/test/java/com/restassured/AppTest.java
@@ -0,0 +1,58 @@
+package com.restassured;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testGetGreeting() {   
+        when().get("https://apichallenges.eviltester.com/sim/entities").
+        then().statusCode(200).contentType(ContentType.JSON);
+        System.out.println();
+    }
+
+    @Test
+    public void testGetASingleEntity() { 
+        given().
+        when().get("https://apichallenges.eviltester.com/sim/entities/1").
+        then().statusCode(200).contentType(ContentType.JSON);
+    }
+
+
+    @Test
+    public void testGetASingleEntityWhichDoesnotexist() { 
+        given().
+        when().get("https://apichallenges.eviltester.com/sim/entities/13").
+        then().statusCode(404).contentType(ContentType.JSON);
+        
+    }
+
+
+    @Test
+    public void testPostASingleEntity() { 
+        given().body("{\"name\": \"bob\"}").
+        when().post("https://apichallenges.eviltester.com/sim/entities").
+        then().statusCode(201).contentType(ContentType.JSON).
+        body("name", equalTo("bob"));
+        
+    }
+
+    @Test
+    public void testDeleteASingleEntity() { 
+        given().
+        when().get("https://apichallenges.eviltester.com/sim/entities/11").
+        then().statusCode(200).contentType(ContentType.JSON);
+        
+    }
+}
